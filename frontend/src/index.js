8e1d2a1d6f4c262555be1dc5034dd7db41e1e225
egorlitin <egorulitin@gmail.com>
2022-11-05 17:16:08 +0300

    add: routing, authorization, redirect without authorization, style invalid auth

--- frontend/src/index.js
+++
@@ -1,9 +0,0 @@
-import { createRoot } from 'react-dom/client';
-import React from 'react';
-import './index.css';
-
-import Component from './components/App.jsx';
-
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<Component />);
